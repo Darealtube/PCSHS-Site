65891d0d56bd519020355772057cb3dbc6188299
Darealtube <68178430+Darealtube@users.noreply.github.com>
2021-10-02 10:03:10 +0800

    Delete prisma/migrations directory

--- prisma/migrations/20210926024928_pcshs_website_2/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Announcement" ADD COLUMN     "authorId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Announcement" ADD CONSTRAINT "Announcement_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Member"("id") ON DELETE SET NULL ON UPDATE CASCADE;
