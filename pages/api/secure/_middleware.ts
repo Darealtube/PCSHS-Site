cdaa48e54f1de363108d4d5d707a022e868bd1b3
Darryl Javier <evajavier143@gmail.com>
2021-12-23 14:15:11 +0800

    Middleware Test

--- pages/api/secure/_middleware.ts
+++ pages/api/secure/_middleware.ts
@@ -6,18 +6,20 @@ const secret = process.env.AUTH_CLIENT_SECRET;
 
 const authenticateUser: NextMiddleware = async (req, _ev) => {
   const token = req.cookies["next-auth.session-token"];
+
+  console.log(req.cookies);
   try {
     const user = await decode({ token, secret: secret as string });
     if (user && user?.role == "Government") {
       return NextResponse.next();
     } else {
-      return new Response(JSON.stringify({message: "403 Forbidden."}), {
+      return new Response(JSON.stringify({ message: "403 Forbidden." }), {
         status: 403,
         statusText: "Forbidden Access.",
       });
     }
   } catch (error) {
-    return new Response(JSON.stringify({message: "401 Unauthenticated"}), {
+    return new Response(JSON.stringify({ message: "401 Unauthenticated" }), {
       status: 401,
       statusText: "Unauthenticated Access.",
     });
